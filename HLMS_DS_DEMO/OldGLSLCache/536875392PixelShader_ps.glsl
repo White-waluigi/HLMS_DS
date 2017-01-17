4d65dcc4ab4778bd02f9e8201088faa5b6fc24bd
Marvin <androidtest.csb@gmail.com>
2017-01-17 20:03:24 +0100

    Chache

---
+++ HLMS_DS_DEMO/OldGLSLCache/536875392PixelShader_ps.glsl
@@ -0,0 +1,341 @@
+//Datablock:	
+
+
+
+//Gbuffer Material
+
+
+
+#version 400 core
+#extension GL_ARB_shading_language_420pack: require
+#extension GL_EXT_texture_array : enable
+
+
+vec4 cubic(float v){
+    vec4 n = vec4(1.0, 2.0, 3.0, 4.0) - v;
+    vec4 s = n * n * n;
+    float x = s.x;
+    float y = s.y - 4.0 * s.x;
+    float z = s.z - 4.0 * s.y + 6.0 * s.x;
+    float w = 6.0 - x - y - z;
+    return vec4(x, y, z, w) * (1.0/6.0);
+}
+vec4 textureBicubic(sampler2D sampler, vec2 texCoords,vec2 texSize){
+
+
+   vec2 invTexSize = 1.0 / texSize;
+   
+   texCoords = texCoords * texSize - 0.5;
+
+   
+    vec2 fxy = fract(texCoords);
+    texCoords -= fxy;
+
+    vec4 xcubic = cubic(fxy.x);
+    vec4 ycubic = cubic(fxy.y);
+
+    vec4 c = texCoords.xxyy + vec2(-0.5, +1.5).xyxy;
+    
+    vec4 s = vec4(xcubic.xz + xcubic.yw, ycubic.xz + ycubic.yw);
+    vec4 offset = c + vec4(xcubic.yw, ycubic.yw) / s;
+    
+    offset *= invTexSize.xxyy;
+    
+    vec4 sample0 = texture(sampler, offset.xz);
+    vec4 sample1 = texture(sampler, offset.yz);
+    vec4 sample2 = texture(sampler, offset.xw);
+    vec4 sample3 = texture(sampler, offset.yw);
+
+    float sx = s.x / (s.x + s.y);
+    float sy = s.z / (s.z + s.w);
+
+    return mix(
+       mix(sample3, sample2, sx), mix(sample1, sample0, sx)
+    , sy);
+}
+
+
+
+
+
+
+
+vec4 rainbow(float phase)
+{
+	float p=mod(phase,1.0);
+	float c=p*3.0;
+	float level = c;
+
+	float x=mod(c,1.0);
+	float y=1-mod(c,1.0);
+
+	float r,g,b;
+	if(level<1){
+		r=y;
+		g=x;
+		b=0;
+	}else if(level<2){
+		r=0;
+		g=y;
+		b=x;
+	}else if(level<3){
+		r=x;
+		g=0;
+		b=y;
+	}
+
+	return vec4(r,g,b,0);
+}
+
+
+
+
+layout(binding = 1) uniform MaterialBuffer
+{
+	//usefull for finding out which materials have the same material block and a way to have materials without params, which glsl doesn't allow
+	vec4 idColor;
+	
+	
+
+
+
+
+/**/
+
+
+
+} material;
+
+
+
+
+struct ShadowData{
+		vec4 size;
+		vec4 lightID;
+		vec4 depthrange;
+		mat4 ShadowCamMat;
+};  
+
+
+layout(binding = 0) uniform PassBuffer
+{
+	vec4 debug;
+	
+	mat4 Proj;
+	
+	mat4 View;
+	
+	mat4 InvView;
+	
+	vec4 time;
+	
+		
+	float nearClip;
+	float farClip;
+	float screenx;
+	float screeny;
+	
+	vec4 farCorners;
+	
+	vec4 flip;
+	
+	float near;
+	float far;
+	float Invdepthrange;
+	float depthrange;
+	
+	vec4 test;
+	
+
+	
+} pass;
+
+
+
+
+
+
+
+
+layout(binding = 0) uniform samplerBuffer worldMatBuf;
+
+
+
+//Uniforms that change per Item/Entity
+layout(binding = 2) uniform InstanceBuffer
+{
+    //.x =
+	//The lower 9 bits contain the material's start index.
+    //The higher 23 bits contain the world matrix start index.
+    //
+    //.y =
+    //shadowConstantBias. Send the bias directly to avoid an
+    //unnecessary indirection during the shadow mapping pass.
+    //Must be loaded with uintBitsToFloat
+    uvec4 worldMaterialIdx[4096];
+} instance;
+
+//layout(binding = 4) uniform indexBuffer
+//{
+//	uvec4 colour; //kD.w is alpha_test_threshold
+//	uvec4 viewProj0;
+//	uvec4 viewProj1;
+//	uvec4 viewProj2;
+//	uvec4 viewProj3;
+	
+//} test;
+
+//layout(binding = 2) uniform InstanceBuffer
+//{
+    //.x =
+	//The lower 9 bits contain the material's start index.
+    //The higher 23 bits contain the world matrix start index.
+    //
+    //.y =
+    //shadowConstantBias. Send the bias directly to avoid an
+    //unnecessary indirection during the shadow mapping pass.
+    //Must be loaded with uintBitsToFloat
+    //uvec4 worldMaterialIdx[4096];
+//} instance;
+
+in block
+{
+
+        flat uint drawId;
+		vec4 pos;
+		vec3 normal;
+		vec3 vertex;
+		vec4 position;
+		vec3 tangent;
+		vec4 worldPos;
+		vec4 glPosition;
+		float depth;
+				
+			
+		vec2 uv0;		
+				
+
+} inPs;
+in vec4 vcolor;
+
+
+out vec4 diffuse;
+out vec4 normal;
+out vec4 pos;
+out vec4 specular;
+out vec4 glow;
+
+uint f2u(float f){
+	return floatBitsToUint(f);
+}
+uint f2u(vec4 f){
+	return floatBitsToUint(f)[0];
+}
+void main() {
+	
+	
+
+
+	float opacity=1.0;
+
+	vec2 screenPos=vec2((gl_FragCoord.x/pass.screenx),(gl_FragCoord.y/pass.screeny));
+
+	vec2 texCoord=vec2(screenPos.x,screenPos.y);
+
+
+
+
+	diffuse=vec4(0);
+	normal=vec4(0);
+	specular=vec4(0);
+	glow=vec4(0.1);
+	
+	
+	
+		
+	
+	
+	
+					
+			diffuse.rgb=vec3(1);	
+			
+		
+			
+
+	
+
+	
+
+
+	normal.xyz=normalize(inPs.normal);
+	normal.w=1.0;
+
+			
+
+	
+					
+			specular=vec4(vec3(0),32.0);	
+			
+	
+
+	
+		
+		glow.rgb=vec3(0);	
+		
+	
+
+	
+
+
+		
+							
+			
+				
+
+	
+		
+		float reflection=0.5;	
+		
+	
+
+	
+	
+
+	
+		
+	normal.w=vec4((length(inPs.pos.xyz) / pass.farClip)).a;
+	//Ogre Shadows want different depth than DS lighting
+	//Linear depth
+	pos.x= (inPs.glPosition.z ) ;
+
+
+
+ 	
+
+
+
+
+
+	
+
+
+
+
+	
+		
+												
+		
+			
+		
+	
+	
+	diffuse.a=0.9;
+
+	
+	if(floatBitsToUint(pass.debug.x)==9u){
+ 		glow=material.idColor;	
+ 	}
+ }
+ 
+ 
+
---
+++ HLMS_DS_DEMO/OldGLSLCache/537923712VertexShader_vs.glsl
@@ -0,0 +1,251 @@
+#version 330 core
+#extension GL_ARB_shading_language_420pack: require
+
+out gl_PerVertex
+{
+	vec4 gl_Position;
+};
+
+layout(std140) uniform;
+
+
+mat4 UNPACK_MAT4( samplerBuffer matrixBuf, uint pixelIdx )
+{
+        vec4 row0 = texelFetch( matrixBuf, int((pixelIdx) << 2u) );
+        vec4 row1 = texelFetch( matrixBuf, int(((pixelIdx) << 2u) + 1u) );
+        vec4 row2 = texelFetch( matrixBuf, int(((pixelIdx) << 2u) + 2u) );
+        vec4 row3 = texelFetch( matrixBuf, int(((pixelIdx) << 2u) + 3u) );
+    return mat4( row0.x, row1.x, row2.x, row3.x,
+                 row0.y, row1.y, row2.y, row3.y,
+                 row0.z, row1.z, row2.z, row3.z,
+                 row0.w, row1.w, row2.w, row3.w );
+}
+
+
+mat4x3 UNPACK_MAT4x3( samplerBuffer matrixBuf, uint pixelIdx )
+{
+        vec4 row0 = texelFetch( matrixBuf, int((pixelIdx) << 2u) );
+        vec4 row1 = texelFetch( matrixBuf, int(((pixelIdx) << 2u) + 1u) );
+        vec4 row2 = texelFetch( matrixBuf, int(((pixelIdx) << 2u) + 2u) );
+        return mat4x3( row0.x, row1.x, row2.x,
+                       row0.y, row1.y, row2.y,
+                       row0.z, row1.z, row2.z,
+                       row0.w, row1.w, row2.w );
+}
+
+
+in vec4 vertex;
+
+in vec4 qtangent;
+
+
+
+
+
+    
+
+in vec2 uv0; 
+in uint drawId;
+
+
+
+out block
+{
+
+        flat uint drawId;
+		vec4 pos;
+		vec3 normal;
+		vec3 vertex;
+		vec4 position;
+		vec3 tangent;
+		vec4 worldPos;
+		vec4 glPosition;
+		float depth;
+		
+			flat float biNormalReflection;
+				
+			
+		vec2 uv0;		
+				
+
+} outVs;
+
+// START UNIFORM DECLARATION
+
+struct ShadowData{
+		vec4 size;
+		vec4 lightID;
+		vec4 depthrange;
+		mat4 ShadowCamMat;
+};  
+
+
+layout(binding = 0) uniform PassBuffer
+{
+	vec4 debug;
+	
+	mat4 Proj;
+	
+	mat4 View;
+	
+	mat4 InvView;
+	
+	vec4 time;
+	
+		
+	float nearClip;
+	float farClip;
+	float screenx;
+	float screeny;
+	
+	vec4 farCorners;
+	
+	vec4 flip;
+	
+	float near;
+	float far;
+	float Invdepthrange;
+	float depthrange;
+	
+	vec4 test;
+	
+
+	
+} pass;
+
+
+layout(binding = 0) uniform samplerBuffer worldMatBuf;
+
+
+
+
+
+vec3 xAxis( vec4 qQuat )
+{
+	float fTy  = 2.0 * qQuat.y;
+	float fTz  = 2.0 * qQuat.z;
+	float fTwy = fTy * qQuat.w;
+	float fTwz = fTz * qQuat.w;
+	float fTxy = fTy * qQuat.x;
+	float fTxz = fTz * qQuat.x;
+	float fTyy = fTy * qQuat.y;
+	float fTzz = fTz * qQuat.z;
+
+	return vec3( 1.0-(fTyy+fTzz), fTxy+fTwz, fTxz-fTwy );
+}
+
+
+vec3 yAxis( vec4 qQuat )
+{
+	float fTx  = 2.0 * qQuat.x;
+	float fTy  = 2.0 * qQuat.y;
+	float fTz  = 2.0 * qQuat.z;
+	float fTwx = fTx * qQuat.w;
+	float fTwz = fTz * qQuat.w;
+	float fTxx = fTx * qQuat.x;
+	float fTxy = fTy * qQuat.x;
+	float fTyz = fTz * qQuat.y;
+	float fTzz = fTz * qQuat.z;
+
+	return vec3( fTxy-fTwz, 1.0-(fTxx+fTzz), fTyz+fTwx );
+}
+
+ 
+
+
+
+
+
+
+
+
+out vec4 vcolor;
+void main()
+{
+
+	outVs.drawId=drawId; 
+	mat4 final;
+
+	mat4 testm=mat4(	1.358,0,0,0,
+						0, 2.41421, 0, 0,
+						0, 0, -1.0004, -0.40008,
+						0, 0, -1, 0 );
+	
+
+
+
+    mat4 worldMat = UNPACK_MAT4( worldMatBuf, drawId<<1);
+	
+    mat4 worldView = UNPACK_MAT4( worldMatBuf, (drawId<<1) + 1u );
+	
+	//vec4	worldPos = vec4( (worldView*vertex) );
+	
+	
+	
+
+ 
+	vec4	worldPos;
+ 
+ 
+ 	worldPos.xyz =  vec4( (worldMat * vertex) ).xyz;
+ 	worldPos.w =  1.0;
+ 	
+  
+ 	
+	outVs.worldPos= worldPos;
+
+
+	
+
+		
+	
+	vcolor =vec4(0.5,float(drawId)/100.0,mod(float(drawId)/10.0,1.0),0);
+	vcolor=worldPos;
+	vcolor=worldPos;
+	
+	
+
+
+
+	
+		outVs.uv0 = uv0;    
+    
+    outVs.vertex=vertex.xyz;
+
+	//Decode qTangent to TBN with reflection
+	vec3 normal		= xAxis( normalize( qtangent ) );
+	vec3 tangent	= yAxis( qtangent );
+	outVs.biNormalReflection = sign( qtangent.w ); //We ensure in C++ qtangent.w is never 0
+		
+		
+
+  
+  outVs.tangent	= mat3(worldView) * tangent;
+  
+
+
+
+	
+		worldPos.w=0;
+		outVs.pos		=pass.View*worldPos;
+	
+		worldPos.w=1;
+		outVs.pos.w=1;
+	    outVs.normal	= mat3(worldView) * normal;
+
+    gl_Position = pass.Proj *(outVs.pos);
+
+	outVs.glPosition =gl_Position;
+
+        vcolor=vertex;
+    
+    
+
+		
+	    
+
+
+
+		
+    }
+    
